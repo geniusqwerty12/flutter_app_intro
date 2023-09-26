8df9a2510456c1c62986bd1213ccbc7d55ac3ec5
John Cristopher Mateo <johncristopher.mateo@wvsu.edu.ph>
2023-09-26 23:57:42 +0800

    Initial commit

---
+++ lib/widgets/customWidget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatelessWidget {
+  const RootApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("assets/images/flutter_sample_image.jpg"),
+    );
+  }
+}
